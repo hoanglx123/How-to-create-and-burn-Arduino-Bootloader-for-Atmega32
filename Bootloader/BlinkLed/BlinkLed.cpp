87040a7d270412295488e15e3d09d60329758181
hoangcoder <nguyenthanhhoang7604119@gmail.com>
2016-05-07 23:38:08 +0700

    Create project

---
+++ Bootloader/BlinkLed/BlinkLed.cpp
@@ -0,0 +1,20 @@
+/*
+ * BlinkLed.cpp
+ *
+ * Created: 4/22/2016 10:38:47 PM
+ *  Author: Mr Hoang
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+	DDRA  = 0xFF; 
+	PORTA = 0xFF; 
+	
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
